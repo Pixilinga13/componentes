cb0723532954943dcd5fbf759ab6516b50b1f2b0
Pixilinga13 <samuelfunec@gmail.com>
2026-09-16 14:27:03 -0300

    commit

---
+++ dispatcher/dispatcher.php
@@ -0,0 +1,8 @@
+<?php
+
+function dispatcher($rota){
+    echo "5. dispatcher decidiu qual controller deve executar.<br>";
+    if ($rota == "/usuarios"){
+        usuarioController();
+    }
+}
---
+++ services/usuario_service.php
@@ -0,0 +1,11 @@
+<?php
+
+function usuarioService(){
+    echo "7. Service esta executando a regra de negocio";
+
+    return [
+        "João",
+        "Maria",
+        "Carlos"
+    ];
+}
